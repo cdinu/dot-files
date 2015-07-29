f6a7ffc5702d3e78a57033ff019ba451a6126caf
Cristian Dinu <cristian.dinu@learnfwd.com>
2015-07-29 00:37:08 +0000

    Small adjustment and fixes

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ These are my configuration files. For now VIM is enough
 In the future I plan to have the scripts to install required npm packages for develpment as well. For now these are:
 
 ```
-docker git iojs
+vim mc  docker git iojs
 
 ```
 npm i -g jshint jscs webpack webpack-dev-server
--- install.sh
+++ install.sh
@@ -5,15 +5,17 @@ read
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-for i in .vimrc .vimrc.local .vimrc.bundle .vimrc.bundle.local .jshintrc .jscsrc .zshrc .tmux.conf bin
+for i in .gitconfig .vimrc .vimrc.local .vimrc.bundles .vimrc.bundles.local .jshintrc .jscsrc .zshrc .tmux.conf bin
 do
   rm -f ~/$i
-  ln $PWD/$i ~/$i
+  ln -s $PWD/$i ~/$i
 done
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-vim +BundleInstall +qall
-cd ~/.vim/bundle/tern_for_vim && npm install
-echo >> ~/.vimrc
 
+vim +BundleInstall +qall
+
+cd ~/.vim/bundle/tern_for_vim && npm install
+
+echo >> ~/.vimrc.local
 echo "colorscheme Tomorrow-Night-Eighties" >> ~/.vimrc.local
